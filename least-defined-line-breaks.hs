030253e40b286348bcdce8b9bc4e0b8ecded29a9
Bryan Richter <b@chreekat.net>
2019-11-12 20:54:03 +0200

    Rework section on optimal subsolns

--- least-defined-line-breaks.hs
+++ least-defined-line-breaks.hs
@@ -1,6 +1,20 @@
-import Data.List
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+-- just for documentationG
+{-# LANGUAGE PackageImports #-}
+import Control.Arrow
+import Control.Monad
+import Data.Foldable
 import Data.Function (on, fix)
-import Data.Function.Memoize
+import Data.List
+import Data.Map (Map, (!))
+import Data.Semigroup
+import Data.Set (Set)
+import "pretty-simple" Debug.Pretty.Simple
+import "pretty-simple" Text.Pretty.Simple
+import qualified Data.List.NonEmpty as N
+import qualified Data.Map as M
+import qualified Data.Set as S
 
 {-
 
@@ -58,8 +72,7 @@ data Cost = Cost Int | MaxCost
 -- | Add two costs
 (.+.) :: Cost -> Cost -> Cost
 Cost x  .+. Cost y  = Cost (x + y)
-_       .+. MaxCost = MaxCost
-MaxCost .+. _       = MaxCost
+_       .+. _       = MaxCost
 
 -- | Cost of a single line
 cost :: Int -> String -> Cost
@@ -126,30 +139,117 @@ last problem, and you're done."
 The real way to do it is to repeat all of that process *for each possible 'last'
 subproblem*, and then choose the best overall solution.
 
-The last step to dynamic programming is to reuse previous results. If we search
-forward from smaller results, the optimal substructure will ensure that we
-continuously reach for the smaller results as we build up larger ones.
+EDIT!
 
-Let's build an algorithm that shows this. Rather than building all possible
-solutions and finding their costs, we'll find solutions as we progress through
-larger and larger subsolutions
+Turns out I was correct all along! The problem is that I am still not properly
+seeing how to formulate the problem such that it has optimal substructure. My
+"improvement" over my old memory was merely an attempt to make this formulation.
+
+Here's another attempt at describing the problem with its proper substructure:
+
+The minimum raggedness for a text with N words is the subsolution for the first
+M-1 words plus the cost of the line comprised of words M..N, where M \elem
+[1..N] is chosen to minimize the overall cost. That is, for word N, you must
+search all M \elem [1..N] and choose the one that has the best score.
+
+That's the last step. Since we have to do it for each word index w \elem [1..N],
+the algorithmic complexity is O(N^2). (We can improve on this, later.)
+
+I know, that's still a terrible description. I'll work on it.
 
 -}
 
-optimalSubSolns :: Int -> [String] -> [String]
-optimalSubSolns _ []  = []
-optimalSubSolns t wss = memoFix f wss
-  where
-    f _ [] = []
-    f step ws =
-        let n         = length ws
-            is        = take n $ inits ws
-            ts        = take n $ tails ws
-            subSolns  = map step is
-            lastLines = map unwords ts
-            combine (ss, l) = let qwe = ss ++ [l] in (ordinarySumCost t qwe, qwe)
-        in  snd
-            . minimumBy (compare `on` fst)
-            . map combine
-            . zip subSolns
-            $ lastLines
+-- | A Break is the index of the last word of a line. I.e. a 0 would mean break
+-- after the first word.
+newtype Break = Break { unBreak :: Int }
+    deriving (Eq, Ord, Num, Show)
+
+data Soln = S { solnCost :: Cost, solnSoln :: [Break] }
+    deriving Show
+
+unshift :: [a] -> a -> [a]
+unshift xs x = xs ++ [x]
+
+minimumOn :: (Ord b, Foldable t) => (a -> b) -> t a -> a
+minimumOn f = minimumBy (compare `on` f)
+
+-- | Implement slice on lists. Sorry.
+slice :: Int -> Int -> [a] -> [a]
+slice i n = take n . drop i
+
+-- | Slice an inclusive segment from a list.
+segment :: Int -> Int -> [a] -> [a]
+segment s e = slice s (e - s + 1)
+
+-- | Just return each solution. We won't try to get tricky with the last line
+-- yet.
+--optimalSubSolns :: Int -> [String] -> [Soln]
+--optimalSubSolns len [] = []
+optimalSubSolns len (N.fromList -> xs) =
+    let n = N.length xs
+        -- | slacks[i,j] is how much slack space exists for a line that starts
+        -- at word i and ends at word j, inclusive
+        slacks :: Map (Int, Int) Int
+        slacks =
+            M.fromList
+                [ ((i,j), slack)
+                | i <- [0..n-1]
+                , j <- [i..n-1]
+                , let slack = len - (length (unwords (segment i j (N.toList xs))))
+                ]
+
+        -- | Mapping slacks to costs.
+        costs :: Map (Int, Int) Cost
+        costs = fmap costfn slacks
+            where costfn n | n < 0     = MaxCost
+                           | otherwise = Cost (n ^ 2)
+
+        -- | Solve given previous solutions
+        -- solve :: [Soln] -> Int -> [Soln]
+        solve nxt (Break j)
+            | j == (-1) = [S (Cost 0) []]
+            | otherwise =
+                let -- | A miracle occurs
+                    previousSolns = nxt (Break (j-1))
+                    -- | These are the possible lines following the possible last
+                    -- breaks
+                    someLines = S.fromList [(i,j) | i <- [0..j]]
+                    -- | These are the costs for those possible lines
+                    someCosts = M.toList (M.restrictKeys costs someLines)
+                    -- Pair up the costs and previous solns so we can pick one
+                    pairs = zipWith combine someCosts previousSolns
+                    -- Given a previous solution and the line that would come
+                    -- after it, create a new solution that adds the line.
+                    combine (_, c) (S c' bs) = S (c' .+. c) (bs ++ [Break j])
+
+                    best = minimumOn solnCost pairs
+                in -- pTraceShow
+                   --  ("prevSolns", previousSolns, "someCosts", someCosts, "pairs", pairs, "best", best) $
+                    previousSolns ++ [best]
+    in fix solve (Break (n-1))
+
+{-
+
+Hooray!! Thanks to laziness, this is already a zillion times faster than the
+brute force method. But it's still VERY slow. Time to start profiling!
+
+-}
+
+
+wrap :: [Break] -> [String] -> [String]
+wrap [] ws = [unwords ws]
+wrap (Break b:bs) ws =
+    let (unwords -> line, rest) = splitAt b ws
+        decr x (Break y) = Break (y - x)
+    in line : wrap (map (decr b) bs) rest
+
+main :: IO ()
+main =
+    let
+        -- txt = words
+        --     $ "My favorite text is usually comprised of many letters and words, given that this is how you comprise text."
+        --     ++ " My favorite text is usually comprised of many letters and words, given that this is how you comprise text."
+        --     ++ " My favorite text is usually comprised of many letters and words, given that this is how you comprise text."
+        --     ++ " My favorite text is usually comprised of mmmmmmmany letters and words, given that this is how you comprise text."
+        txt = replicate 800 "aaa"
+    in putStrLn $ unlines $ flip wrap txt $ solnSoln $ last $ optimalSubSolns 30 $ txt
